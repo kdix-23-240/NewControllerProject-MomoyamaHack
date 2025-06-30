ffec842612d3da18550bee465813e1d48e08365f
kdix-23-240 <akihide.3kami@gmail.com>
2025-06-30 13:28:04 +0900

    ステージセレクトのGriConの位置調整

--- Assets/Script/Developper/CameraLay.cs
+++ Assets/Script/Developper/CameraLay.cs
@@ -5,11 +5,11 @@ public class CameraLay : MonoBehaviour
     void Update()
     {
         // 6ï˚å¸Ç…å¸ÇØÇƒê‘Ç¢ê¸Çï`âÊ
-        Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
-        Debug.DrawRay(transform.position, -transform.forward * 10f, Color.red);
-        Debug.DrawRay(transform.position, transform.right * 10f, Color.red);
-        Debug.DrawRay(transform.position, -transform.right * 10f, Color.red);
-        Debug.DrawRay(transform.position, transform.up * 10f, Color.red);
-        Debug.DrawRay(transform.position, -transform.up * 10f, Color.red);
+        Debug.DrawRay(transform.position, transform.forward * 50f, Color.red);
+        Debug.DrawRay(transform.position, -transform.forward * 50f, Color.red);
+        Debug.DrawRay(transform.position, transform.right * 50f, Color.red);
+        Debug.DrawRay(transform.position, -transform.right * 50f, Color.red);
+        Debug.DrawRay(transform.position, transform.up * 50f, Color.red);
+        Debug.DrawRay(transform.position, -transform.up * 50f, Color.red);
     }
 }
